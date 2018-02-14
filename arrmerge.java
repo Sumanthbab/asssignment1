e641d7d8343ab58a59af0170367e4b1fa46fbbb9
Sumanthbab <36447296+Sumanthbab@users.noreply.github.com>
2018-02-14 20:33:13 +0530

    Create arrmerge.java

---
+++ arrmerge.java
@@ -0,0 +1,18 @@
+public static void mergeArrays(int[] arr1, int[] arr2, int n1,int n2)
+{
+  int i = 0, j = 0, k = 0;
+  int arr3[]=new int[];
+  while (i<n1 && j <n2)
+  {
+    if (arr1[i] < arr2[j])
+      arr3[k++] = arr1[i++];
+    else
+     arr3[k++] = arr2[j++];
+  }
+   while (i < n1)
+     arr3[k++] = arr1[i++];
+   while (j < n2)
+     arr3[k++] = arr2[j++];
+}
+
+
